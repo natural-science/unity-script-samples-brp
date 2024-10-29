526c26d47218203bfd414ec538d748c0817e651e
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2024-10-29 22:20:02 +0900

    .

---
+++ Assets/20241025_MonoBehaviourEssentials/HelloLocalVars.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace _20241025_MonoBehaviourEssentials
+{
+  public class HelloLocalVars : MonoBehaviour
+  {
+    private int a = 1;
+    private int b = 2;
+
+    void Start()
+    {
+      Debug.Log($"A: {a}, {b}");
+      {
+        Debug.Log($"B: {a}, {b}");
+        {
+          a = 5;
+          Debug.Log($"C: {a}, {b}");
+          {
+            a = 6;
+            int b = 7;
+            Debug.Log($"D: {a}, {b}");
+          }
+          a = 8;
+          Debug.Log($"E: {a}, {b}");
+        }
+        {
+          int b = 9;
+          Debug.Log($"F: {a}, {b}");
+        }
+        Debug.Log($"G: {a}, {b}");
+      }
+      Debug.Log($"H: {a}, {b}");
+    }
+  }
+}
---
+++ Assets/20241025_MonoBehaviourEssentials/HelloVarsInvalid.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace _20241025_MonoBehaviourEssentials
+{
+  public class HelloVarsInvalid : MonoBehaviour
+  {
+    public int a = 99;
+    public int b = 88;
+
+    void Start()
+    {
+#if false
+      Debug.Log($"A: {a}, {b}");
+      {
+        Debug.Log($"B: {a}, {b}"); // C# ではだめ
+        a = 9999; // C# ではだめ
+        Debug.Log($"C: {a}, {b}"); // C# ではだめ
+        int a = 11; // C# ではだめ
+        int b = 22; // C# ではだめ
+        Debug.Log($"D: {a}, {b}");
+        {
+          int a = 33; // C# ではだめ
+          Debug.Log($"E: {a}, {b}");
+          {
+            a = 333;
+            int b = 44; // C# ではだめ
+            Debug.Log($"F: {a}, {b}");
+          }
+          a = 3333;
+          Debug.Log($"G: {a}, {b}");
+        }
+        {
+          int b = 55; // C# ではだめ
+          Debug.Log($"H: {a}, {b}");
+        }
+        Debug.Log($"I: {a}, {b}");
+      }
+      Debug.Log($"J: {a}, {b}");
+#endif
+    }
+  }
+}
