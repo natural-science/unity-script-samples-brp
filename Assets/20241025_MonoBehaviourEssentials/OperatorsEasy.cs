5f3769260dd6e64632bdf27c59368cf81b43ffb9
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2024-10-29 22:52:26 +0900

    .

---
+++ Assets/20241025_MonoBehaviourEssentials/OperatorsEasy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace _20241025_MonoBehaviourEssentials
+{
+  public class OperatorsEasy : MonoBehaviour
+  {
+    void Start()
+    {
+      int a = 0;
+      int b = 0;
+      Debug.Log($"A: {a}, {b}");
+      b = a + 1;
+      Debug.Log($"B: {a}, {b}");
+      a = a + 1;
+      Debug.Log($"C: {a}, {b}");
+      a += b;
+      Debug.Log($"D: {a}, {b}");
+      b -= a;
+      Debug.Log($"E: {a}, {b}");
+      a++;
+      Debug.Log($"F: {a}, {b}");
+      --b;
+      Debug.Log($"G: {a}, {b}");
+
+      Debug.Log($"%: {0 % 3}, {1 % 3}, {2 % 3}, {3 % 3}, {4 % 3}, {5 % 3}, {6 % 3}, {7 % 3}");
+      Debug.Log($"==: {0 == 3}, {1 == 3}, {2 == 3}, {3 == 3}, {4 == 3}, {5 == 3}");
+      Debug.Log($"!=: {0 != 3}, {1 != 3}, {2 != 3}, {3 != 3}, {4 != 3}, {5 != 3}");
+      Debug.Log($"<: {0 < 3}, {1 < 3}, {2 < 3}, {3 < 3}, {4 < 3}, {5 < 3}");
+      Debug.Log($"<=: {0 <= 3}, {1 <= 3}, {2 <= 3}, {3 <= 3}, {4 <= 3}, {5 <= 3}");
+      Debug.Log($"&&: {false && false}, {false && true}, {true && false}, {true && true}");
+      Debug.Log($"||: {false || false}, {false || true}, {true || false}, {true || true}");
+      Debug.Log($"!: {!false}, {!true}");
+      Debug.Log($"?:: {(true ? 1 : 0)} {(false ? 1 : 0)}");
+    }
+  }
+}
---
+++ Assets/20241025_MonoBehaviourEssentials/OperatorsMedium.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace _20241025_MonoBehaviourEssentials
+{
+  public class OperatorsMedium : MonoBehaviour
+  {
+    void Start()
+    {
+      int a = 0;
+      int b = 0;
+      Debug.Log($"A: {a}, {b}");
+      b = a++;
+      Debug.Log($"B: {a}, {b}");
+      b = ++a;
+      Debug.Log($"C: {a}, {b}");
+      a = b = 3;
+      Debug.Log($"D: {a}, {b}");
+
+      Debug.Log($"E: {false && true || false} {true && true || false}");
+      Debug.Log($"F: {false || true && false} {true || true && false}");
+      Debug.Log($"G: {false || true && !false} {!true || true && false}");
+      Debug.Log($"H: {!!!!false} {!!!!true}");
+      a = 2;
+      var s =
+        a == 0 ? "zero" :
+        a == 1 ? "one" :
+        a == 2 ? "two" :
+        a == 3 ? "three" : "many";
+      Debug.Log($"I: {s}");
+    }
+  }
+}
