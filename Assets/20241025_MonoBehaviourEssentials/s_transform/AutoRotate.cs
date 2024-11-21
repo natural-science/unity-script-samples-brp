831eb43e4f878b47528bd5091e95a1d9a7e9dd03
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2024-11-22 02:39:51 +0900

    .

---
+++ Assets/20241025_MonoBehaviourEssentials/s_transform/AutoRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _20241025_MonoBehaviourEssentials
+{
+  public class AutoRotate : MonoBehaviour
+  {
+    void Update()
+    {
+      transform.rotation = Quaternion.AngleAxis(1f, new Vector3(0f, 1f, 0f)) * transform.rotation;
+    }
+  }
+}
--- Assets/20241025_MonoBehaviourEssentials/s_transform/TestTransform.cs
+++ Assets/20241025_MonoBehaviourEssentials/s_transform/TestTransform.cs
@@ -4,6 +4,13 @@ namespace _20241025_MonoBehaviourEssentials
 {
   public class TestTransform : MonoBehaviour
   {
+    [SerializeField] private GameObject _go0;
+    [SerializeField] private GameObject _go1;
+    [SerializeField] private GameObject _go2;
+
+    private float timer = 0f;
+    private int target = 0;
+
     void Update()
     {
       Debug.Log($"@@@");
@@ -14,6 +21,21 @@ namespace _20241025_MonoBehaviourEssentials
       Debug.Log($"  rotation = {transform.rotation}");
       Debug.Log($"  localRotation = {transform.localRotation}");
       Debug.Log($"  localScale = {transform.localScale}");
+
+
+      timer -= 0.01f;
+      if (timer < 0f) {
+        timer = 1f;
+        target = (target + 1) % 3;
+      }
+
+      if (target == 0) {
+        transform.position += (_go0.transform.position - transform.position) * 0.01f;
+      } else if (target == 1) {
+        transform.position += (_go1.transform.position - transform.position) * 0.01f;
+      } else {
+        transform.position += (_go2.transform.position - transform.position) * 0.01f;
+      }
     }
   }
 }
